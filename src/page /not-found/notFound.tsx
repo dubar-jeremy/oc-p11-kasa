529ddefd28d8738f597663412d2356ed74908944
Dubar Jérémy <jeremy.dubar@decathlon.com>
2023-10-15 00:19:36 +0200

    feat: not found page integration

---
+++ src/page /not-found/notFound.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import notFoundLogo from '../../assets/404.png'
+import './notFound.css'
+
+const NotFound = () => {
+    return (
+        <div className="not_found">
+            <img src={notFoundLogo} alt="not found" className='not_found_logo' />
+            <p className='not_found_text'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to={"/"} className='not_found_link'>Retourner sur la page d’accueil</Link>
+        </div>
+    )
+}
+
+export default NotFound
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import Home from "../page /home/Home";
 import Housing from "../page /housing/housing";
+import NotFound from "../page /not-found/notFound";
 
 
 const AppRouter = () => {
@@ -11,8 +12,8 @@ const AppRouter = () => {
                 <Route index element={<Home />} />
                 <Route path="housing/:id" element={<Housing />} />
                 <Route path="about" element={<p>about</p>}/>
-                <Route path="*" element={<p>Not found</p>} />
-                <Route path="not-found" element={<p>Not found</p>} />
+                <Route path="*" element={<NotFound/>} />
+                <Route path="not-found" element={<NotFound />} />
             </Route>
         </Routes>
     );
